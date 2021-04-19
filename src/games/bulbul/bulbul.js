0938cfaeb9bf6c19dd73eb8f0d5f7c1a11a5eccd
Liubov Kvasova <bubochka102003@gmail.com>
2021-04-19 15:13:36 +0300

    Added player in bulbul

--- src/games/bulbul/bulbul.js
+++ src/games/bulbul/bulbul.js
@@ -1,8 +1,8 @@
 //canvas
 const canvas = document.querySelector('#canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = 800;
-canvas.height = 500;
+canvas.width = 900;
+canvas.height = 600;
 
 let score = 0;
 let gameFrame = 0;
@@ -13,9 +13,51 @@ const mouse = {
     y: canvas.height / 2,
     click: false
 };
-canvas.addEventListener('mousedown', function(event) {
+canvas.addEventListener('mousedown', function(event){
     mouse.x = event.x - canvasPosition.left;
     mouse.y = event.y - canvasPosition.top;
 });
 
+//player
+class Player {
+    constructor(){
+        this.x = canvas.width;
+        this.y = canvas.height/2;
+        this.radius = 60;
+    }
+    update(){
+        const dx = this.x - mouse.x;
+        const dy = this.y - mouse.y;
+        if (mouse.x != this.x){
+            this.x -= dx / 30;
+        }
+        if (mouse.y != this.y){
+            this.y -= dy / 30;
+        }
+    }
+    draw(){
+        if(mouse.click) {
+            ctx.lineWidth = 0.1;
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+        }
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.fillRect(this.x, this.y, this.radius, 0);
+    }
+}
+const player = new Player();
 
+//animation
+function animation(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    player.draw();
+    requestAnimationFrame(animation);
+}
+animation();
