3cd4654418d66e29b417c23a91c6fa013b4b4041
Liubov Kvasova <bubochka102003@gmail.com>
2021-05-31 14:02:50 +0300

    edit spaces

--- src/games/bulbul/bulbul.js
+++ src/games/bulbul/bulbul.js
@@ -23,49 +23,49 @@ gameOverMusic.src = './sound/gameover.mp3';
 //mouse
 let canvasPosition = canvas.getBoundingClientRect();
 const mouse = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
+  x: canvas.width / 2,
+  y: canvas.height / 2,
 };
-canvas.addEventListener('mousemove', function(event){
-    mouse.x = event.x - canvasPosition.left;
-    mouse.y = event.y - canvasPosition.top;
+canvas.addEventListener('mousemove', function (event) {
+  mouse.x = event.x - canvasPosition.left;
+  mouse.y = event.y - canvasPosition.top;
 });
 
 //player
 const imgPlayer = new Image();
 imgPlayer.src = './img/player.png';
 class Player {
-    constructor(){
-        this.x = canvas.width;
-        this.y = canvas.height/2;
-        this.radius = 60;    
+  constructor() {
+    this.x = canvas.width;
+    this.y = canvas.height / 2;
+    this.radius = 60;
+  }
+  update() {
+    const dx = this.x - mouse.x;
+    const dy = this.y - mouse.y;
+    if (mouse.x != this.x) {
+      this.x -= dx / 15;
     }
-    update(){
-        const dx = this.x - mouse.x;
-        const dy = this.y - mouse.y;
-        if (mouse.x != this.x){
-            this.x -= dx / 15;
-        }
-        if (mouse.y != this.y){
-            this.y -= dy / 15;
-        }
+    if (mouse.y != this.y) {
+      this.y -= dy / 15;
     }
-    draw(){
-        if(mouse.move) {
-            ctx.lineWidth = 0.1;
-            ctx.beginPath();
-            ctx.moveTo(this.x, this.y);
-            ctx.lineTo(mouse.x, mouse.y);
-            ctx.stroke();
-        }
-        ctx.fillStyle = 'rgba(219, 204, 68)';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
-        ctx.fillRect(this.x, this.y, this.radius, 0);
-        ctx.drawImage(imgPlayer, this.x - 80, this.y - 80, 150, 150);
+  }
+  draw() {
+    if (mouse.move) {
+      ctx.lineWidth = 0.1;
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.stroke();
     }
+    ctx.fillStyle = 'rgba(219, 204, 68)';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+    ctx.fillRect(this.x, this.y, this.radius, 0);
+    ctx.drawImage(imgPlayer, this.x - 80, this.y - 80, 150, 150);
+  }
 }
 const player = new Player();
 
@@ -74,128 +74,129 @@ const bulArray = [];
 const imgBul = new Image();
 imgBul.src = './img/bul.png';
 class Bul {
-    constructor (){
-        this.x = Math.random() * canvas.width;
-        this.y = canvas.height + 100;
-        this.radius = 50;
-        this.speed = Math.random() * 6 + 1;  
-        this.distance = 0;   
-        this.count = false;   
-        this.sound = 'sound';
-    }
-    update(){
-        this.y -= this.speed;
-        const dx = this.x - player.x;
-        const dy = this.y - player.y;
-        this.distance = Math.sqrt(dx * dx + dy * dy);
-    }
-    draw(){
-        ctx.fillStyle = '#161c22';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
-        ctx.stroke();
-        ctx.drawImage(imgBul, this.x - 50, this.y - 50, 100, 100);
-    }
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = canvas.height + 100;
+    this.radius = 50;
+    this.speed = Math.random() * 6 + 1;
+    this.distance = 0;
+    this.count = false;
+    this.sound = 'sound';
+  }
+  update() {
+    this.y -= this.speed;
+    const dx = this.x - player.x;
+    const dy = this.y - player.y;
+    this.distance = Math.sqrt(dx * dx + dy * dy);
+  }
+  draw() {
+    ctx.fillStyle = '#161c22';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+    ctx.drawImage(imgBul, this.x - 50, this.y - 50, 100, 100);
+  }
 }
 
-function handlyBul(){
-    if (gameFrame % 50 == 0){
-        bulArray.push(new Bul());
+function handlyBul() {
+  if (gameFrame % 50 == 0) {
+    bulArray.push(new Bul());
+  }
+  for (let i = 0; i < bulArray.length; i++) {
+    if (bulArray[i].y < 0) {
+      skip++;
+      bulArray.splice(i, 1);
     }
-    for (let i = 0; i < bulArray.length; i++){
-        if (bulArray[i].y < 0){
-            skip++;
-            bulArray.splice(i, 1);
-        }
-          bulArray[i].update();
-          bulArray[i].draw();
-        if (bulArray[i].distance < bulArray[i].radius + player.radius){
-            if (!bulArray[i].count){
-                bulMusic.play();
-                score++;
-                bulArray[i].count = true;
-                bulArray.splice(i, 1);
-            }
-        }
+    bulArray[i].update();
+    bulArray[i].draw();
+    if (bulArray[i].distance < bulArray[i].radius + player.radius) {
+      if (!bulArray[i].count) {
+        bulMusic.play();
+        score++;
+        bulArray[i].count = true;
+        bulArray.splice(i, 1);
+      }
     }
+  }
 }
 
 //danger fish
 const dangerFishImage = new Image();
 dangerFishImage.src = './img/dangerFish.png';
 class DangerFish {
-    constructor(){
-        this.x = 0;
-        this.y = Math.random() * canvas.height;
-        this.radius = 50;
-        this.speed = Math.random() * 2 + 2;
+  constructor() {
+    this.x = 0;
+    this.y = Math.random() * canvas.height;
+    this.radius = 50;
+    this.speed = Math.random() * 2 + 2;
+  }
+  draw() {
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.drawImage(dangerFishImage, this.x - 130, this.y - 130, 200, 200);
+  }
+  update() {
+    this.x += this.speed;
+    if (this.x > canvas.width - this.radius * 2) {
+      this.x = 0;
+      this.y = Math.random() * canvas.height;
+      this.speed = Math.random() * 2 + 3;
     }
-    draw(){
-        ctx.fillStyle = 'black';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.drawImage(dangerFishImage, this.x - 130, this.y - 130, 200, 200);
+    const dx = this.x - player.x;
+    const dy = this.y - player.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < this.radius + player.radius) {
+      lives--;
     }
-    update(){
-        this.x += this.speed;
-        if (this.x > canvas.width - this.radius * 2){
-            this.x = 0;
-            this.y = Math.random() * canvas.height;
-            this.speed = Math.random() * 2 + 3;
-        }
-        const dx = this.x - player.x;
-        const dy = this.y - player.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-        if (distance < this.radius + player.radius){
-            lives--;
-        }
-    }
+  }
 }
 const dangerFish1 = new DangerFish();
-function handlyDangerfish(){
-     dangerFish1.draw();
-    dangerFish1.update();
+
+function handlyDangerfish() {
+  dangerFish1.draw();
+  dangerFish1.update();
 }
 
 //inscription on canvas
-function writeInCtx (){
-    ctx.fillStyle = 'white';
-    ctx.fillText(`lives: ${lives - skip}♥ ` , 20, 80);
-    ctx.fillText('score:' + score, 20, 40);
+function writeInCtx() {
+  ctx.fillStyle = 'white';
+  ctx.fillText(`lives: ${lives - skip}♥ `, 20, 80);
+  ctx.fillText('score:' + score, 20, 40);
 }
 
-function gameOver (){
-        allTotal += Math.round(score/10);
-        localStorage.setItem('allTotal', JSON.stringify(allTotal));
-        document.getElementById('allTotal').value = allTotal;
-        bulArray.count = true;
-        for (let i = 0; i < bulArray.length; i++){
-            bulArray.splice(i);
-        }
-        repeat.style.visibility='visible';
-        gameOverMusic.play();
+function gameOver() {
+  allTotal += Math.round(score / 10);
+  localStorage.setItem('allTotal', JSON.stringify(allTotal));
+  document.getElementById('allTotal').value = allTotal;
+  bulArray.count = true;
+  for (let i = 0; i < bulArray.length; i++) {
+    bulArray.splice(i);
+  }
+  repeat.style.visibility = 'visible';
+  gameOverMusic.play();
 }
 
 //animation
-function animation(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    repeat.style.visibility='hidden';
-    handlyBul();
-    if (score >= 15) handlyDangerfish();
-    player.update();
-    player.draw();
-    writeInCtx();
-    if (skip == lives){
-        gameOver();
-        return;
-    }
-    gameFrame++;
-    requestAnimationFrame(animation);
+function animation() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  repeat.style.visibility = 'hidden';
+  handlyBul();
+  if (score >= 15) handlyDangerfish();
+  player.update();
+  player.draw();
+  writeInCtx();
+  if (skip == lives) {
+    gameOver();
+    return;
+  }
+  gameFrame++;
+  requestAnimationFrame(animation);
 }
 animation();
-repeat.addEventListener('click', function(){
-    location.href = location.href;
+repeat.addEventListener('click', function () {
+  location.href = location.href;
 });
--- src/todolist/todolist.js
+++ src/todolist/todolist.js
@@ -6,151 +6,146 @@ const finishedTasks = document.getElementById('finished-tasks');
 //change background
 const backgrs = document.querySelectorAll('.background');
 for (const backgr of backgrs) {
-    const id = backgr.id;
-    backgr.addEventListener('click', () => {
-        document.body.style.backgroundImage = `url('/src/todolist/img/${id}.jpg')`;
-    });
-} 
+  const id = backgr.id;
+  backgr.addEventListener('click', () => {
+    document.body.style.backgroundImage = `url('/src/todolist/img/${id}.jpg')`;
+  });
+}
 
 function createNewElement(task, finished) {
-    const listItem = document.createElement('li');
-    const checkbox = document.createElement('button');
+  const listItem = document.createElement('li');
+  const checkbox = document.createElement('button');
 
-    if (finished){
+  if (finished) {
     checkbox.className = "checkbox";
     checkbox.innerHTML = "<i class='icons'>cancel</i>";
-    }else {
+  } else {
     checkbox.className = "checkbox";
     checkbox.innerHTML = "<i class='icons'>✔done</i>";
-    }
+  }
 
-    const label = document.createElement('label');
-    label.innerText = task;
-    const input = document.createElement('input');
-    input.type = "text";
-    const editButton = document.createElement('button');
-    editButton.className = "edit";
-    editButton.innerHTML = "<i class='icons'>edit</i>";
-    const deleteButton = document.createElement('button');
-    deleteButton.className = "delete";
-    deleteButton.innerHTML = "<i class='icons'>delete</i>";
+  const label = document.createElement('label');
+  label.innerText = task;
+  const input = document.createElement('input');
+  input.type = "text";
+  const editButton = document.createElement('button');
+  editButton.className = "edit";
+  editButton.innerHTML = "<i class='icons'>edit</i>";
+  const deleteButton = document.createElement('button');
+  deleteButton.className = "delete";
+  deleteButton.innerHTML = "<i class='icons'>delete</i>";
 
-    listItem.appendChild(checkbox);
-    listItem.appendChild(label);
-    listItem.appendChild(input);
-    listItem.appendChild(deleteButton);
-    listItem.appendChild(editButton);
+  listItem.appendChild(checkbox);
+  listItem.appendChild(label);
+  listItem.appendChild(input);
+  listItem.appendChild(deleteButton);
+  listItem.appendChild(editButton);
 
-    return listItem;
+  return listItem;
 }
 
 function addTask() {
-    if (inputTask.value) {
-        const listItem = createNewElement(inputTask.value, false);
-        unfinishedTasks.appendChild(listItem);
-        bindTaskEvents(listItem, finishTask);
-        inputTask.value = "";
-    }
-    save();
+  if (inputTask.value) {
+    const listItem = createNewElement(inputTask.value, false);
+    unfinishedTasks.appendChild(listItem);
+    bindTaskEvents(listItem, finishTask);
+    inputTask.value = "";
+  }
+  save();
 }
 addButton.onclick = addTask;
 
 function deleteTask() {
-    const listItem = this.parentNode;
-    const ul = listItem.parentNode;
-    ul.removeChild(listItem);
-    save();
+  const listItem = this.parentNode;
+  const ul = listItem.parentNode;
+  ul.removeChild(listItem);
+  save();
 }
 
 function editTask() {
-    const editButton = this;
-    const listItem = this.parentNode;
-    const label = listItem.querySelector('label');
-    const input = listItem.querySelector('input[type=text]');
+  const editButton = this;
+  const listItem = this.parentNode;
+  const label = listItem.querySelector('label');
+  const input = listItem.querySelector('input[type=text]');
 
-    const containsClass = listItem.classList.contains('editMode');
+  const containsClass = listItem.classList.contains('editMode');
 
-    if (containsClass) {
-        label.innerText = input.value;
-        editButton.className = "edit";
-        editButton.innerHTML = "<i class='icons'>edit</i>";
-        save();
-    } else {
-        input.value = label.innerText;
-        editButton.className = "save";
-        editButton.innerHTML = "<i class='icons'>save</i>";
+  if (containsClass) {
+    label.innerText = input.value;
+    editButton.className = "edit";
+    editButton.innerHTML = "<i class='icons'>edit</i>";
+    save();
+  } else {
+    input.value = label.innerText;
+    editButton.className = "save";
+    editButton.innerHTML = "<i class='icons'>save</i>";
 
-    }
-    listItem.classList.toggle('editMode');
+  }
+  listItem.classList.toggle('editMode');
 }
 
 function finishTask() {
-    const listItem = this.parentNode;
-    const checkbox = listItem.querySelector('button.checkbox');
-    checkbox.className = "checkbox";
-    checkbox.innerHTML = "<i class='icons'>cancel</i>";
-    finishedTasks.appendChild(listItem);
-    bindTaskEvents(listItem, unfinishTask);
-    save();
+  const listItem = this.parentNode;
+  const checkbox = listItem.querySelector('button.checkbox');
+  checkbox.className = "checkbox";
+  checkbox.innerHTML = "<i class='icons'>cancel</i>";
+  finishedTasks.appendChild(listItem);
+  bindTaskEvents(listItem, unfinishTask);
+  save();
 }
 
 function unfinishTask() {
-    const listItem = this.parentNode;
-    const checkbox = listItem.querySelector('button.checkbox');
-    checkbox.className = "checkbox";
-    checkbox.innerHTML = "<i class='icons'>✔done</i>";
+  const listItem = this.parentNode;
+  const checkbox = listItem.querySelector('button.checkbox');
+  checkbox.className = "checkbox";
+  checkbox.innerHTML = "<i class='icons'>✔done</i>";
 
-    unfinishedTasks.appendChild(listItem);
-    bindTaskEvents(listItem, finishTask);
-    save();
+  unfinishedTasks.appendChild(listItem);
+  bindTaskEvents(listItem, finishTask);
+  save();
 }
 
 function bindTaskEvents(listItem, checkboxEvent) {
-    const checkbox = listItem.querySelector('button.checkbox');
-    const editButton = listItem.querySelector('button.edit');
-    const deleteButton = listItem.querySelector('button.delete');
+  const checkbox = listItem.querySelector('button.checkbox');
+  const editButton = listItem.querySelector('button.edit');
+  const deleteButton = listItem.querySelector('button.delete');
 
-    checkbox.onclick = checkboxEvent;
-    editButton.onclick = editTask;
-    deleteButton.onclick = deleteTask;
+  checkbox.onclick = checkboxEvent;
+  editButton.onclick = editTask;
+  deleteButton.onclick = deleteTask;
 
 }
+
 function save() {
-
-    const unfinishedTasksArr = [];
-    for (let i = 0; i < unfinishedTasks.children.length; i++) {
-        unfinishedTasksArr.push(unfinishedTasks.children[i].getElementsByTagName('label')[0].innerText);
-    }
-
-    const finishedTasksArr = [];
-    for (let i = 0; i < finishedTasks.children.length; i++) {
-        finishedTasksArr.push(finishedTasks.children[i].getElementsByTagName('label')[0].innerText);
-    }
-
-    localStorage.removeItem('todo');
-    localStorage.setItem('todo', JSON.stringify({
-        unfinishedTasks: unfinishedTasksArr,
-        finishedTasks: finishedTasksArr
-    }));
-
+  const unfinishedTasksArr = [];
+  for (let i = 0; i < unfinishedTasks.children.length; i++) {
+    unfinishedTasksArr.push(unfinishedTasks.children[i].getElementsByTagName('label')[0].innerText);
+  }
+  const finishedTasksArr = [];
+  for (let i = 0; i < finishedTasks.children.length; i++) {
+    finishedTasksArr.push(finishedTasks.children[i].getElementsByTagName('label')[0].innerText);
+  }
+  localStorage.removeItem('todo');
+  localStorage.setItem('todo', JSON.stringify({
+    unfinishedTasks: unfinishedTasksArr,
+    finishedTasks: finishedTasksArr
+  }));
 }
 
-function load(){
-    return JSON.parse(localStorage.getItem('todo'));
+function load() {
+  return JSON.parse(localStorage.getItem('todo'));
 }
 
-const data=load();
+const data = load();
 
-for(let i=0; i<data.unfinishedTasks.length;i++){
-    const listItem=createNewElement(data.unfinishedTasks[i], false);
-    unfinishedTasks.appendChild(listItem);
-    bindTaskEvents(listItem, finishTask);
+for (let i = 0; i < data.unfinishedTasks.length; i++) {
+  const listItem = createNewElement(data.unfinishedTasks[i], false);
+  unfinishedTasks.appendChild(listItem);
+  bindTaskEvents(listItem, finishTask);
 }
 
-for(let i=0; i<data.finishedTasks.length; i++){
-    const listItem=createNewElement(data.finishedTasks[i], true);
-    finishedTasks.appendChild(listItem);
-    bindTaskEvents(listItem, unfinishTask);
+for (let i = 0; i < data.finishedTasks.length; i++) {
+  const listItem = createNewElement(data.finishedTasks[i], true);
+  finishedTasks.appendChild(listItem);
+  bindTaskEvents(listItem, unfinishTask);
 }
-
-
