334799b8dc10ac44308caff3d72ff366d5719527
ukioxz <ikatember@gmail.com>
2021-06-01 20:27:47 +0300

    Add weekday quo

--- src/quotes/quotes.js
+++ src/quotes/quotes.js
@@ -1,15 +1,33 @@
 const hideText = document.getElementById("overlay");
 const getText = document.getElementById("help");
 const getQuotes = document.getElementById("getQuo");
-const fs = require("fs");
-const quo = require('./quotes.json');
-fs.readFile('./quotes.json', 'utf8', function (err, data) {
-    if (err) throw err; // we'll not consider error handling for now
-    const obj = JSON.parse(data);
-});
 
-const random = quo.quotes[Math.floor(Math.random() * quo.quotes.length)];
-  
+const day1 = ["Главное в жизни - это уметь думать. Попытайтесь пожалуйста",
+  "Прекрати бежать напрасно, друг мой, ты не обязан мечтать обо всём",
+  "Люди всегда видят лишь то, что хотят видеть",
+  "Не попади в ловушку чужой мечты",
+  "Я говорил, что мы выиграем. Хотя я не верил в это",
+  "Если головоломка не сложилась и тебе не подобрать пазлы, то начинай сначала"
+];
+const day2 = [
+  "Believe in yourself. Your limitation—it's only your imagination.",
+  "Work hard. Push yourself, because no one else is going to do it for you.",
+  "Be yourself; everyone else is already taken.",
+  "Будьте собой и говорите, что чувствуете, потому что те, кто возражает, не имеют значения, а те, кто имеет значение, не возражают"
+];
+const day3 = [
+  "Always forgive your enemies; nothing annoys them so much.",
+  "A friend is someone who knows all about you and still loves you.",
+  "Without music, life would be a mistake.",
+  "Only I can change my life. No one can do it for me.",
+  "Die with memories, not dreams."
+];
+
+function week(){
+  const n = new Date().getDay();
+
+  const random = `day${n}`[Math.floor(Math.random() * `day${n}`.length)];//doesnt wotk returned undefine
+}
 function getGreeting() {
   let greeting;
   const time = new Date().getHours();
@@ -18,17 +36,16 @@ function getGreeting() {
   else if (time < 18) greeting = "Good day";
   else greeting = "Good evening";
 
-  document.getElementById("gr").innerHTML = greeting + " " + random;
+
+  document.getElementById("gr").innerHTML =  greeting + " " + week();
 
   function createNotification() {
     const notif = document.createElement("div");
-    notif.classList.add("toast");
-    notif.innerText = greeting + " " + randomItem(quotes);
+    notif.innerText = greeting + " " + week();
 
     container.appendChild(notif);
   }
 }
-
 function lockoutSubmit(button) {
   const oldValue = button.value;
 
@@ -41,6 +58,7 @@ function lockoutSubmit(button) {
 }
 
 getQuotes.addEventListener("click", () => {
+  week();
   getGreeting();
   createNotification();
   lockoutSubmit(button);
@@ -57,8 +75,8 @@ hideText.addEventListener("click", () => {
 function setImage() {
   const time = new Date().getHours();
 
-  if (time < 14) { document.body.style.backgroundImage = "url('./img/back1.png')"; }
-  else if (time < 19) { document.body.style.backgroundImage = "url('./img/back2.png')"; }
-  else { document.body.style.backgroundImage = "url('./img/back3.png')"; }
+  if (time < 14) document.body.style.backgroundImage = "url('./img/back1.png')";
+  else if (time < 19) document.body.style.backgroundImage = "url('./img/back2.png')";
+  else document.body.style.backgroundImage = "url('./img/back3.png')";
 }
 setImage();
