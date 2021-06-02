8f034530d5b4e428de4d7a4211023a5ac45415f3
Yulechka <julie.lehenka@gmail.com>
2021-06-02 14:24:10 +0300

    improved fishcan

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,26 +1,15 @@
 document.getElementById('bulbul').addEventListener('click', () => {
-  location.href = './games/bulbul/bulbul.html';
+    location.href = './games/bulbul/bulbul.html';
 });
 document.getElementById('fishcan').addEventListener('click', () => {
-  location.href = './games/fish cannibal/fishcan.html';
+    location.href = './games/fish cannibal/fishcan.html';
 });
 document.getElementById('snake').addEventListener('click', () => {
-  location.href = './games/snake/snake.html';
+    location.href = './games/snake/snake.html';
 });
 document.getElementById('tetris').addEventListener('click', () => {
-  location.href = './games/tetris/tetris.html';
+    location.href = './games/tetris/tetris.html';
 });
 document.getElementById('forest').addEventListener('click', () => {
-  location.href = './games/evil forest/forest.html';
-});
-
-
-document.getElementsByClassName('card1').addEventListener('click', () => {
-  location.href = './todolist/todolist.html';
-});
-document.getElementsByClassName('card2').addEventListener('click', () => {
-  location.href = './calendar planner/calendar.html';
-});
-document.getElementsByClassName('card3').addEventListener('click', () => {
-  location.href = './quotes/quotes.html';
+    location.href = './games/evil forest/forest.html';
 });
