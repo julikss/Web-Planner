e9263c98ae5a6488fd3030c4e88063d178b395ed
Liubov Kvasova <bubochka102003@gmail.com>
2021-06-04 16:29:35 +0300

    Fixed shitcode in mainpage.js

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,15 +1,15 @@
-document.getElementById('bulbul').addEventListener('click', () => {
-  location.href = './games/bulbul/bulbul.html';
-});
-document.getElementById('fishcan').addEventListener('click', () => {
-  location.href = './games/fish cannibal/fishcan.html';
-});
-document.getElementById('snake').addEventListener('click', () => {
-  location.href = './games/snake/snake.html';
-});
-document.getElementById('tetris').addEventListener('click', () => {
-  location.href = './games/tetris/tetris.html';
-});
-document.getElementById('forest').addEventListener('click', () => {
-  location.href = './games/evil forest/forest.html';
-});
+const elementMatrix = [
+  ['bulbul', 'bulbul/bulbul'],
+  ['fishcan', 'fish cannibal/fishcan'],
+  ['snake', 'snake/snake'],
+  ['tetris', 'tetris/tetris'],
+  ['forest', 'evil forest/forest'],
+];
+
+for (const element of elementMatrix) {
+  const id = element[0];
+  const pathPart = element[1];
+  document.getElementById(id).addEventListener('click', () => {
+    location.href = './games/' + pathPart + '.html';
+  });
+}
